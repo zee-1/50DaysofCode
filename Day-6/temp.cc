d8dc7a1ddb6e0e5500eba8ee1dd507563a4bf4e9
zee-1 <zaid.khan21800@gmail.com>
2023-08-22 21:21:49 +0530

    Done by Zee-1 File updated at $TIME

---
+++ Day-6/temp.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+      int day=6;
+      while(day<50){
+            std::cout<<"Keep pushing you limits"<<std::endl;
+      }
+      return 0;
+}
