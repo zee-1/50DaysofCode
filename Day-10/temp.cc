2fcd01144161d652b7aee548b86cbbee063fb336
zee-1 <zaid.khan21800@gmail.com>
2023-08-24 23:53:23 +0530

    Added a new file

--- Day-10/temp.cc
+++ Day-10/temp.cc
@@ -1,2 +1,12 @@
 #include <iostream>
-using
+using namespace std;
+int main(int argc, char const *argv[])
+{
+      int day=10;
+      while (day!=50)
+      {
+            std::cout<<"Grind Your skills"<<std::endl;
+      }
+      
+      return 0;
+}
