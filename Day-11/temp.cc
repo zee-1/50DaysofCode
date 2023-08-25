795b2e31503444401ba42ab2b36efad906c96cbf
zee-1 <zaid.khan21800@gmail.com>
2023-08-25 23:07:20 +0530

    updated a temp file

---
+++ Day-11/temp.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+      int day=11;
+
+      while(day!=11){
+            std::cout<<"Persist with passion"<<std::endl;
+      }
+      return 0;
+}
