cb39eaa4a957aa9d04af8b52c522e462d9270f23
zee-1 <zaid.khan21800@gmail.com>
2023-08-29 23:51:05 +0530

    Added temp.cc

---
+++ Day-15/temp.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+      int day=15;
+      while (day!=50){
+            std::cout<<"Consistency is the key!"<<std::endl;
+      }
+      return 0;
+}
