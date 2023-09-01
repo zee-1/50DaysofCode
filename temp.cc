f6ded22c3bb83d4e780a854496b38eb847354aa0
zee-1 <zaid.khan21800@gmail.com>
2023-09-01 22:44:41 +0530

    Added another garbage++

---
+++ temp.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+      int day=18;
+      while(day!=50){
+            std::cout<<"Push past your limit"<<std::endl;
+      }
+      return 0;
+}
