d2c4053547aac8602e4c5d2a4c5ff2f4fdf06c5d
zee-1 <zaid.khan21800@gmail.com>
2023-09-01 22:14:17 +0530

    Added garbage

---
+++ Day-18/constVec.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// class foo{
+//       public:
+//             foo(){
+//                   std::cout<<"Scammed"<<std::endl;
+//             }
+//       private:
+//             foo();
+
+//       int addd(int,int);
+// };
+
+
+// void bar(foo){
+//       std::cout<<"bar called"<<std::endl;
+// }
+
+int main(int argc, char const *argv[])
+{
+      char arr[100];
+      while(std::cin>>arr ){
+            std::cout<<arr<<std::endl;
+
+      }
+      return 0;
+}
