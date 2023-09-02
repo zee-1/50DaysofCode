2e3ff9bb9970ffa404bf39c4365fb7477ce35022
zee-1 <zaid.khan21800@gmail.com>
2023-09-02 20:55:44 +0530

    file: Added garbage file

---
+++ Day-19/temp.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+      int day=19;
+      while(day!=50){
+            std::cout<<"Keep on improving"<<std::endl;
+      }
+      return 0;
+}
