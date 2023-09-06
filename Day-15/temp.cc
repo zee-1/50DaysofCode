d3fb58c32e0c975f7d141dca242b4c15d0c4dfe1
zee-1 <zaid.khan21800@gmail.com>
2023-09-06 23:22:23 +0530

    idk

--- Day-15/temp.cc
+++ Day-15/temp.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-      int day=15;
+      int day=22;
       while (day!=50){
             std::cout<<"Consistency is the key!"<<std::endl;
       }
