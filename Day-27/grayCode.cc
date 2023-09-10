1718e984ac7450d50704def4644f6d7669e5e30f
zee-1 <zaid.khan21800@gmail.com>
2023-09-10 22:26:48 +0530

    File: grayCode.cc

---
+++ Day-27/grayCode.cc
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using std::vector;
+using std::max;
+using std::min;
+using std::sort;
+using std::swap;
+using std::reverse;
+
+void 
+
+int main(int argc, char const *argv[])
+{
+    int n=0;
+    std::cin>>n;
+
+return 0;
+}
